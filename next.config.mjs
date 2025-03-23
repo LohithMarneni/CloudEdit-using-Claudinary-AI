dcb1a1d9d2cfce85f546cf5fedf334f1bdb62361
Rohantunikipati <rohantunikipat@gamil.com>
2025-03-23 11:30:36 +0530

    fixed the fiexd errors

--- next.config.mjs
+++ next.config.mjs
@@ -5,22 +5,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        port: '',
-      },
-    ],
-  },
-  experimental: {
-    appDir: true, // Ensure app directory support is enabled
-  },
-  output: 'standalone', // Helps in Vercel deployments
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false, // Prevents Next.js from trying to use Node.js modules in Edge Runtime
-      net: false,
-      tls: false,
-    };
-    return config;
-  },
+        port: ''
+      }
+    ]
+  }
 };
 
 export default nextConfig;
